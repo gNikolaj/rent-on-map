7de8e3ad8df473019c9a98d647183e19dc764bd8
gniko <gnikowork@gmail.com>
2022-08-26 19:12:36 +0300

    Deploy changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,7 @@ import AdsList from "../ads-list/AdsList";
 import Map, {MODES} from "../map/Map";
 import {getAds, postAd} from "../../api";
 
-import {API_KEY, defaultCenter, libraries} from "../../utils";
+import { defaultCenter, libraries} from "../../utils";
 
 import './App.css';
 import SearchInput from "../search-input/SearchInput";
@@ -22,7 +22,7 @@ const App = () => {
 
     const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: API_KEY,
+        googleMapsApiKey: 'AIzaSyAyJnDs_s80VtU0DPKAr8bNfDahtQx65qU',
         libraries
     });
 
