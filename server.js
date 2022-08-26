e8585e1c88b1a6e75e48dcb0a08bdf3aee2f4db3
gniko <gnikowork@gmail.com>
2022-08-26 19:27:35 +0300

    Deploy changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
     static: './build',
 });
@@ -8,6 +8,9 @@ const middlewares = jsonServer.defaults({
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router);
 
 server.listen(port);
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const getAds = async () => {
-    const {data} = await axios.get('http://localhost:8080/places');
+    const {data} = await axios.get('https://rent-on-map.herokuapp.com/api/places');
     return data;
 }
 
 export const postAd = (product) => {
-    return axios.post('http://localhost:8080/places', product);
+    return axios.post('https://rent-on-map.herokuapp.com/api/places', product);
 }
